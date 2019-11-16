173c18e7e46b62e270abb4b5906a8d2d0445a4eb
emir <emir.jumaliev@gmail.com>
2019-11-16 03:32:22 -0500

    was added LOGIN_URL,LOGIN_REDIRECT_URL,LOGOUT_REDIRECT_URl

--- source/main/settings.py
+++ source/main/settings.py
@@ -120,5 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_URL = "accounts:login"
+LOGIN_REDIRECT_URL = "webapp:index"
+LOGOUT_REDIRECT_URL = "webapp:index"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = '/uploads/'
