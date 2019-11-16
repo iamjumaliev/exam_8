56f69999f4542a36b27f931fd2ed4bcb9585ad71
emir <emir.jumaliev@gmail.com>
2019-11-16 02:33:13 -0500

    was made forms for users

---
+++ source/accounts/forms.py
@@ -0,0 +1,156 @@
+from django.contrib.auth.models import User
+
+from django import forms
+from django.core.exceptions import ValidationError
+
+from accounts.models import UserProfile
+
+
+class UserCreationForm(forms.ModelForm):
+
+    password = forms.CharField(label="Пароль", strip=False,
+                               widget=forms.PasswordInput(attrs={'placeholder': u'Enter password'}))
+    password_confirm = forms.CharField(label="Подтвердите пароль",
+                                       widget=forms.PasswordInput(attrs={'placeholder': u'Enter password confirm'}),
+                                        strip=False)
+    email = forms.EmailField(label='email', required=True,
+                             widget=forms.EmailInput(attrs={'placeholder': u'Enter email'}))
+
+    def clean_password_confirm(self):
+
+        password = self.cleaned_data.get("password")
+
+        password_confirm = self.cleaned_data.get("password_confirm")
+
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError('Пароли не совпадают!')
+
+        return password_confirm
+
+    def save(self, commit=True):
+
+        user = super().save(commit=False)
+
+        user.set_password(self.cleaned_data["password"])
+
+        if commit:
+            user.save()
+
+        user.save()
+
+        UserProfile.objects.create(user=user)
+
+        return user
+
+    def clean(self):
+        super().clean()
+        first_name = self.cleaned_data.get('first_name')
+        last_name = self.cleaned_data.get('last_name')
+        if not first_name and not last_name:
+            raise ValidationError('Error, first or last name must be filled', code='name_is_empty')
+        return self.cleaned_data
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        try:
+            User.objects.get(email=email)
+            raise ValidationError('Email already registered.', code='email_registered')
+        except User.DoesNotExist:
+            return email
+
+    class Meta:
+
+        model = User
+
+        fields = ['username', 'password', 'password_confirm', 'first_name', 'last_name', 'email']
+
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': u'Enter username'}),
+            'first_name': forms.TextInput(attrs={'placeholder': u'Enter first name'}),
+            'last_name': forms.TextInput(attrs={'placeholder': u'Enter first name'})
+        }
+
+class UserChangeForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['github'] = forms.URLField(required=False)
+        self.fields['avatar'] = forms.ImageField()
+        self.fields['about'] = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+        profile_fields = ['avatar', 'about', 'github']
+        labels = {'first_name': 'Имя', 'last_name': 'Фамилия', 'email': 'Email'}
+
+    def get_initial_for_field(self, field, field_name):
+        if field_name in self.Meta.profile_fields:
+            return getattr(self.instance.profile, field_name)
+        return super().get_initial_for_field(field, field_name)
+
+    def save(self, commit=True):
+        user = super().save(commit=commit)
+        user.profile = self.save_profile(commit)
+        return user
+
+    def save_profile(self, commit=True):
+        profile, _ = UserProfile.objects.get_or_create(user=self.instance)
+        for field in self.Meta.profile_fields:
+            setattr(profile, field, self.cleaned_data.get(field))
+        if not profile.avatar:
+            profile.avatar = None
+        if commit:
+            profile.save()
+        return profile
+
+    def clean_github(self):
+        gl = self.cleaned_data.get('github')
+        g = 'github.com'
+        l = ''
+        if l == gl:
+            return gl
+        elif gl.find(g) in range(0,11):
+            print(gl.find(g))
+            return gl
+        else:
+            raise forms.ValidationError('error')
+
+class UserChangePasswordForm(forms.ModelForm):
+    password = forms.CharField(max_length=100, required=True, label='New Password',
+                               widget=forms.PasswordInput)
+    password_confirm = forms.CharField(max_length=100, required=True, label='New Password confirm',
+                                       widget=forms.PasswordInput)
+    old_password = forms.CharField(max_length=100, required=True, label='Old Password',
+                                   widget=forms.PasswordInput)
+
+    def clean_old_password(self):
+        old_password = self.cleaned_data.get('old_password')
+        user = self.instance
+        if not user.check_password(old_password):
+            raise ValidationError('Invalid password.', code='invalid_password')
+        return old_password
+
+
+
+    def clean(self):
+        super().clean()
+        password_1 = self.cleaned_data.get('password')
+        password_2 = self.cleaned_data.get('password_confirm')
+        if password_1 != password_2:
+            raise ValidationError('Passwords do not match.', code='passwords_do_not_match')
+        return self.cleaned_data
+
+    def save(self, commit=True):
+        user = self.instance
+        user.set_password(self.cleaned_data.get('password'))
+        if commit:
+            user.save()
+        return user
+
+    class Meta:
+        model = User
+        fields = ['password', 'password_confirm', 'old_password']
+
+
+
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,3 +1,85 @@
-from django.shortcuts import render
+from accounts.forms import UserChangeForm
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from django.views.generic import DetailView, UpdateView, ListView
+
+from accounts.models import UserProfile
+from .forms import UserCreationForm, UserChangePasswordForm
+
+
+
+def login_view(request,StatisticsMixin):
+    context = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('webapp:index')
+        else:
+            context['has_error'] = True
+    return render(request, 'registration/login.html', context=context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('webapp:login')
+
+def register_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('webapp:index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'user_create.html', context={'form': form})
+
+class UserDetailView(DetailView):
+    model = User
+    template_name = 'user_detail.html'
+    context_object_name = 'user_obj'
+
+
+
+class UserChangeView(UserPassesTestMixin, UpdateView):
+    model = User
+    template_name = 'user_update.html'
+    context_object_name = 'user_obj'
+    form_class = UserChangeForm
+
+    def test_func(self):
+        return self.get_object() == self.request.user
+
+
+    def get_success_url(self):
+        return reverse('accounts:user_detail', kwargs={'pk': self.object.pk})
+
+
+
+class UserChangePasswordView(UserPassesTestMixin, UpdateView):
+    model = User
+    template_name = 'user_change_password.html'
+    form_class = UserChangePasswordForm
+    context_object_name = 'user_obj'
+
+    def test_func(self):
+        return self.get_object() == self.request.user
+
+    def get_success_url(self):
+        return reverse('accounts:login')
+
+
+class UsersList(ListView):
+    template_name = 'users_list.html'
+    context_object_name = 'users'
+    model = User
+    paginate_by = 3
+    paginate_orphans = 1
 
 # Create your views here.
