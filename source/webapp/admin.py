f489499288604353680f2f7c16baa7eb1ba3ed87
emir <emir.jumaliev@gmail.com>
2019-11-16 01:01:28 -0500

    Was configuered admin

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib import admin
+from webapp.models import Product, Review
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category')
+    list_filter = ('category')
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'product')
+    list_filter = ('rating')
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Review,ReviewAdmin)
 # Register your models here.
